3d01a87ac07d46ca7c581d73ac05a28f4ff2b67e
Ignazio Gandolfo <ignzioentertainment@gmail.com>
2023-07-19 22:10:30 +0100

    fix config for prodution #2

--- next.config.js
+++ next.config.js
@@ -1,6 +1,11 @@
 /** @type {import('next').NextConfig} */
 const nextConfig = {
     reactStrictMode: false,
+    output: 'export',
+    images: {
+        loader: 'custom',
+        loaderFile: './app/image.ts',
+      },
 }
 
 module.exports = nextConfig
