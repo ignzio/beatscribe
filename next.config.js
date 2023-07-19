00fa9f598ae45e0b49f23f67a4f5f43a57374b71
Ignazio Gandolfo <ignzioentertainment@gmail.com>
2023-07-19 21:08:45 +0100

    githud deploy workflow

--- next.config.js
+++ next.config.js
@@ -10,14 +10,7 @@ const nextConfig = {
         }
         return config;
     },
-    // Add the experimental feature if you want to use it, but be aware of the potential risks
-  experimental: {
-    images: true,
-  },
-  // Enable static HTML export
-  // This will generate static HTML files for your Next.js application
-  target: 'experimental-serverless-trace', // Use experimental-serverless-trace to enable static HTML export
-    
+
 }
 
 module.exports = nextConfig
