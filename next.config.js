86cb3a55cd28875cf4debfa9298e2d252ec96f59
Ignazio Gandolfo <ignzioentertainment@gmail.com>
2023-07-19 22:08:03 +0100

    fix config for prodution

--- next.config.js
+++ next.config.js
@@ -1,16 +1,6 @@
 /** @type {import('next').NextConfig} */
 const nextConfig = {
     reactStrictMode: false,
-    images: {
-        domains: ['i.scdn.co'],
-    },
-    webpack: (config, { isServer }) => {
-        if (!isServer) {
-            config.resolve.fallback.fs = false;
-        }
-        return config;
-    },
-
 }
 
 module.exports = nextConfig
